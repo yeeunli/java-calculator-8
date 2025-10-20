76014dbaad315f585bcd0eb2aefca51d5a9a6931
yeeunli <lye920@g.hongik.ac.kr>
2025-10-20 20:51:43 +0900

    feat: 문자열 입력 함수 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 1. 문자열 입력
+        inputString();
+
+    }
+
+    /**
+     * 문자열을 입력받습니다.
+     */
+    public static void inputString() {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        try {
+            str = bufferedReader.readLine();
+        } catch (IOException e) {
+            System.out.println("입력 중 오류가 발생했습니다. : " + e.getMessage());
+        }
     }
 }
