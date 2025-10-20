27f65f8efc4458c75a27a46182e25084a9c5ad4e
yeeunli <lye920@g.hongik.ac.kr>
2025-10-20 22:33:22 +0900

    feat: 커스텀 구분자로 나눈 숫자 추출 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,10 +10,13 @@ public class Application {
     public static void main(String[] args) {
 
         // 1. 문자열 입력
-        String str = inputString();
+        String originalStr = inputString();
 
-        // 2. 숫자 추출
-        extractNum(str);
+        // 2. 구분자 추출
+        ParsedResult result = parseInput(originalStr);
+
+        // 3. 숫자 추출
+        extractNum(result.data(), result.delimiter());
 
     }
 
@@ -37,13 +40,13 @@ public class Application {
     }
 
     /**
-     * 기본 구분자를 기준으로 숫자를 추출합니다.
+     * 구분자를 기준으로 숫자를 추출합니다.
      * @param str 입력받은 문자열
      */
-    public  static void extractNum(String str) {
+    public static void extractNum(String str, String delimiter) {
 
         // 구분자(쉼표, 콜론) 기준으로 나눈다
-        String[] strArr = str.split("[,\\:]");
+        String[] strArr = str.split(delimiter);
 
         // 숫자 하나씩 배열에 담는다
         List<Integer> numList = new ArrayList<>();
@@ -52,5 +55,28 @@ public class Application {
             int num = Integer.parseInt(i.trim());
             numList.add(num);
         }
+    }
+
+    record ParsedResult(String delimiter, String data) {}
+
+    /**
+     * 원본 문자열에서 문자열과 구분자를 추출합니다.
+     * @param originalStr
+     * @return
+     */
+    public static ParsedResult parseInput(String originalStr) {
+
+        // 디폴트 값으로 기본 구분자를 반환한다
+        String delimiter = "[,\\:]";
+        String data = originalStr;
+
+        // 커스텀 구분자를 조건으로 추출한다
+        if (originalStr.startsWith("//") && originalStr.contains("\\n")) {
+            int position = originalStr.indexOf("\\n");
+            delimiter = originalStr.substring(2, position);
+            data = originalStr.substring(position + 2);
+        }
+
+        return new ParsedResult(delimiter, data);
     }
 }
